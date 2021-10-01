684b4de9a93c2261a7307b29ee85a80cc434b6dd
magalipujol <magalicpujol@gmail.com>
2021-10-01 09:53:57 -0300

    created the database

---
+++ js/database.js
@@ -0,0 +1,41 @@
+let recipes = [
+    {
+        sectionName: "Tartas",
+        // ? the ID isn't necessary maybe
+        sectionID: "tartas-content",
+        contents: [
+            {
+                title: "Tarta de berenjenas",
+                price: 10,
+                instructions: "",
+                imageSRC: "",
+                imageAlt: ""
+            }
+        ]
+    },
+    {
+        sectionName: "Pastas",
+        sectionID: "pastas-content",
+        contents: [
+            {
+                title: "Malfatti de espinaca",
+                price: 10,
+                instructions: "",
+                imageSRC: "",
+                imageAlt: "" 
+            }
+        ]
+    },
+    {
+        sectionName: "Ensaladas"
+    },
+    {
+        sectionName: "Sandwiches"
+    },
+    {
+        sectionName: "Postres",
+    },
+    {
+        sectionName: "Bebidas",
+    }
+]
